ca28f0456fef3590065fdc81da971996e4844cd7
Justin Hall <justinhall@macbook-pro-9.lan>
2024-01-29 15:19:34 +0800

    Update CORS configuration

--- server.mjs
+++ server.mjs
@@ -9,9 +9,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: 'https://damp-reaches-11122-434fcd8858d2.herokuapp.com'
-}));
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
